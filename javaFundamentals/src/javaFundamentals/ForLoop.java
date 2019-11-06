4af4d37f29845e99060eb5a2f1d8c003783e7025
unknown <raaga.prathyusha@yahoo.com>
2019-11-06 11:12:49 +0530

    javaFundamentals programs

---
+++ javaFundamentals/src/javaFundamentals/ForLoop.java
@@ -0,0 +1,13 @@
+package javaFundamentals;
+
+public class ForLoop {
+public static void main (String args[])
+{
+	for(int i=0;i<4;i++)
+	{
+	System.out.println("hello susan.." +i);
+	
+	System.out.println("hello java..!");
+	}
+}
+}
